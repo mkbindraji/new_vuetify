d24deca34fb5d59d55bfa7acd23a220a6f31c9e6
mkbindraji <mkbindra2003@gmail.com>
2024-09-19 10:30:46 +0530

    Text & Colours

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="home">
       <h1>HomePage</h1>
+      <p class="red blue--text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas voluptate laborum iste, ducimus sapiente deleniti dignissimos. Exercitationem error aliquam possimus aut temporibus atque beatae labore, corrupti nam minima voluptatibus. Fugit.</p>
+      <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit est culpa, quibusdam eos voluptatum ratione, tempora quod et quam explicabo pariatur sint delectus alias at ipsam ullam repellendus iste rerum.</p>
+      <h1 class="display-4">Dassive Display</h1>
+      <h1 class="display-1">Small Display</h1>
+      <p class="headline">This this headingline</p>
+      <p class="subheading font-weight-bold">This this a subheadingline</p>
+      <p class="caption">This this a caption</p>
     </div>
   </template>
   
