27efb20fcdaa853988502e238c144e8c0caac81f
mkbindraji <mkbindra2003@gmail.com>
2024-09-19 10:59:32 +0530

    Button & Icons

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,13 +1,30 @@
 <template>
     <div class="home">
       <h1>HomePage</h1>
-      <p class="red blue--text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas voluptate laborum iste, ducimus sapiente deleniti dignissimos. Exercitationem error aliquam possimus aut temporibus atque beatae labore, corrupti nam minima voluptatibus. Fugit.</p>
-      <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit est culpa, quibusdam eos voluptatum ratione, tempora quod et quam explicabo pariatur sint delectus alias at ipsam ullam repellendus iste rerum.</p>
-      <h1 class="display-4">Dassive Display</h1>
-      <h1 class="display-1">Small Display</h1>
-      <p class="headline">This this headingline</p>
-      <p class="subheading font-weight-bold">This this a subheadingline</p>
-      <p class="caption">This this a caption</p>
+      <v-btn class="pink white--text">Click me</v-btn><br><br>
+      <v-btn depressed color="pink">Click me</v-btn><br><br>
+      <v-btn flat color="pink">Click me</v-btn><br><br>
+
+      <v-btn depressed class="pink white--text">
+        <v-icon left>mdi-email</v-icon>
+        <span>email me</span>
+      </v-btn><br><br>
+
+      <v-btn depressed small class="pink white--text">
+        <v-icon left small>mdi-email</v-icon>
+        <span>email me</span>
+      </v-btn><br><br>
+
+      <v-btn depressed large class="pink white--text">
+        <span>email me</span>
+        <v-icon right large>mdi-email</v-icon>
+      </v-btn><br><br>
+
+      <v-btn depressed fab dark color="purple" dense>
+        <v-icon left>mdi-heart</v-icon>
+      </v-btn>
+
+
     </div>
   </template>
   
