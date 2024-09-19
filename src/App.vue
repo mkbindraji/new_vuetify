a3b6d7c461a0bb6d0cbbb033374e82cfccab28e6
mkbindraji <mkbindra2003@gmail.com>
2024-09-19 11:51:04 +0530

    Toolbars

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <template>
-  <v-app>
+  <v-app class="grey lighten-4">
+
+    <NavbarPage></NavbarPage>
+
     <v-content>
       <router-view></router-view> <!-- Use "router-view" not "route-view" -->
     </v-content>
+
   </v-app>
 </template>
 
 <script>
+
+import NavbarPage from '@/components/NavbarPage'
+
 export default {
   name: 'App',
-
+  components: {
+    NavbarPage
+  },
   data: () => ({
-    //
-  }),
+    // Add your data properties here
+  })
 };
+
 </script>
 
---
+++ src/components/NavbarPage.vue
@@ -0,0 +1,22 @@
+<template>
+    <nav>
+        <v-toolbar flat app>
+
+            <v-toolbar-title class="text-uppercase grey--text">
+                <span class="font-weight-light">App</span>
+                <span>Vuetify</span>
+            </v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-btn flat color="lighten-2">
+                <span>Sign Out</span>
+                <v-icon right>mdi-exit-to-app</v-icon>
+            </v-btn>
+        </v-toolbar>
+    </nav>
+</template>
+
+<script>
+
+</script>
