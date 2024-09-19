4f98d5086580bfca8562a1eb2608f338a6937e68
mkbindraji <mkbindra2003@gmail.com>
2024-09-19 13:50:14 +0530

    Themes

--- src/components/NavbarPage.vue
+++ src/components/NavbarPage.vue
@@ -1,36 +1,32 @@
 <template>
-    <nav>
-      <!-- Toolbar -->
-      <v-toolbar flat app class="elevation-0 info darken-3 white--text">
-        <!-- Menu Icon for Drawer -->
-        <v-toolbar-side-icon @click="drawer = !drawer">
-          <v-icon class="white--text">mdi-menu</v-icon>
-        </v-toolbar-side-icon>
+    <v-app>
+      <v-app-bar flat app>
   
-        <!-- App Title -->
-        <v-toolbar-title class="text-uppercase">
+        <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer">
+          <v-icon>mdi-menu</v-icon>
+        </v-app-bar-nav-icon>
+  
+        <v-toolbar-title class="text-uppercase grey--text">
           <span class="font-weight-light">App</span>
           <span class="font-weight-bold">Vuetify</span>
         </v-toolbar-title>
   
         <v-spacer></v-spacer>
   
-        <!-- Sign Out Button -->
-        <v-btn text class="white--text">
+        <v-btn variant="text" color="lighten-2">
           <span>Sign Out</span>
           <v-icon right>mdi-exit-to-app</v-icon>
         </v-btn>
-      </v-toolbar>
+      </v-app-bar>
   
-        <!-- Navigation Drawer -->
-        <v-navigation-drawer
-            app
-            v-model="drawer"
-            class="info darken-4 white--text"
-            temporary
-            @click-outside="drawer = false">
-        </v-navigation-drawer>
-    </nav>
+      <v-navigation-drawer app v-model="drawer" class="indigo">
+        <v-list-item class="pa-4">
+          <v-list-item-content>
+            <v-list-item-title class="error">Navigation</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-navigation-drawer>
+    </v-app>
   </template>
   
   <script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,21 @@
 import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vuetify from 'vuetify/lib';
+import 'vuetify/dist/vuetify.min.css'; // Correct CSS import for Vuetify 2.x
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#9652ff',
+        success: '#3cd1c2',
+        info: '#ffaa2c',
+        error: '#f83e70'
+      }
+    }
+  },
+  icons: {
+    iconfont: 'md'
+  }
 });
